58fede1e8227a598cb4b549eed5a38ee5e457da2
zhu3pang <zhujing0227@gmail.com>
2023-08-16 12:03:37 +0800

    init

---
+++ src/main/java/com/example/teamcitydemo/TeamcityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamcitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamcitydemo/TeamcityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamcitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
